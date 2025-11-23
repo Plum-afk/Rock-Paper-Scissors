252d1e996d4813880b6ba2ab168ec9d6db2c1bd5
Plum-afk <24103848@usc.edu.ph>
2025-11-23 17:01:25 +0800

    Fix game connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('RPS Server is running!');
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
